1ff79e63bf4756945bbf97f212a24e90b7096aab
roodj <slutty322@mail.ru>
2024-06-10 23:38:33 +0300

    some shit to test

--- test_temp.py
+++ test_temp.py
@@ -1,6 +1,21 @@
-a,b = [int(input()) for i in range(2)]
-c = a
+# Глобальная область видимости
+global_var = "I'm a global variable"
 
-while c % b:
-    c += a
-print(c)
+def outer_function():
+    # Нелокальная область видимости
+    nonlocal_var = "I'm a nonlocal variable"
+
+    def inner_function():
+        # Локальная область видимости
+        local_var = "I'm a local variable"
+        
+        # Доступ к переменным всех трех областей видимости
+        print(global_var)    # Доступ к глобальной переменной
+        print(nonlocal_var)  # Доступ к нелокальной переменной
+        print(local_var)     # Доступ к локальной переменной
+
+    return inner_function
+
+# Создание и вызов функции
+inner = outer_function()
+inner()
